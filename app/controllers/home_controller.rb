54fbb37b865461dedf35f791fdefd948cfbbdeb8
Kyle Jacobson <ksj@twelve8.net>
2012-08-06 21:05:25 -0700

    redirecting logged in user from home#index to user#show

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,9 @@
 class HomeController < ApplicationController
   def index
+    if current_user
+      redirect_to user_path(current_user) and return
+    end
+
     respond_to do |format|
       format.html # index.html.erb
       format.json  { render :json => @jobs }

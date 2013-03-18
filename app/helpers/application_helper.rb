9b12a75b8fc8efd558cb0acb82ff1f3086816409
Kyle Jacobson <ksj@twelve8.net>
2013-03-17 21:44:21 -0700

    #3 nil_friendly_decode

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,6 +4,14 @@ module ApplicationHelper
     return coder.decode(input)
   end
 
+  def nil_friendly_decode(input)
+    if input.nil? || input.empty?
+      return decode("&mdash;")
+    else
+      return decode(input)
+    end
+  end
+
   def friendly_date(date)
     date.strftime("%b %e, %Y")
   end

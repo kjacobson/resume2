624170b99eea30ccd8d77791cad3a2a8b7ddc2a1
Kyle Jacobson <ksj@twelve8.net>
2013-04-08 23:12:54 -0700

    trying again: only load app_config if file exists, this time in 01_app_config

--- config/initializers/01_app_config.rb
+++ config/initializers/01_app_config.rb
@@ -1,3 +1,3 @@
 require 'app_config'
 
-AppConfig.config = YAML.load_file("config/app_config.yml")[Rails.env].symbolize_keys
+AppConfig.config = YAML.load_file("config/app_config.yml")[Rails.env].symbolize_keys if File.exist?("config/app_config.yml")

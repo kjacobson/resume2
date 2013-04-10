d947c7c3f4784f0f7752aa3af0bb8da0fb4a0200
Kyle Jacobson <ksj@twelve8.net>
2013-04-09 19:50:04 -0700

    allow spaces in skill, software titles

--- app/models/software.rb
+++ app/models/software.rb
@@ -1,6 +1,7 @@
 class Software < ActiveRecord::Base
     validates_presence_of :title
     validates_uniqueness_of :title
+    validates_format_of :title, :with => /\A([a-zA-Z0-9\-\_\+\ ]*)\Z/i
 
     has_many :job_softwares, :dependent => :destroy
     has_many :jobs, :through => :job_softwares

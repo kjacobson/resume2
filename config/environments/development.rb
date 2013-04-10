4e27c54dd0d9bf896e43d191675ca08918be0e7d
Kyle Jacobson <ksj@twelve8.net>
2013-04-09 19:01:22 -0700

    tiny style tweak

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,5 +35,6 @@ Resume2::Application.configure do
   config.assets.compress = false
   # Expands the lines which load the assets
   config.assets.debug = true
+  config.assets.compile = true
 end
 

2e2a7e352c1fc2c233de3dcf4aa666577a06b606
Kyle Jacobson <kylejacobson2009@u.northwestern.edu>
2013-04-10 12:40:57 -0700

    replace usage of link.hash that i missed

--- app/models/link.rb
+++ app/models/link.rb
@@ -10,7 +10,7 @@ class Link < ActiveRecord::Base
     if !resume.nil?
       user_id = resume.user.id
       host = AppConfig.config[:host]
-      return "http://" + host + "/users/#{user_id}/resumes/#{resume.id}?access_key=" + self.hash
+      return "http://" + host + "/users/#{user_id}/resumes/#{resume.id}?access_key=" + self.unique_key
     else
       return ""
     end

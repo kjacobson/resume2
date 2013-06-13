f6ae2824e9f2f48c5868496a30d671b6467ee2a4
Kyle Jacobson <ksj@twelve8.net>
2013-06-12 20:29:28 -0700

    fix #18 add page titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,28 @@ module ApplicationHelper
   def friendly_date(date)
     date.strftime("%b %e, %Y")
   end
+
+  def auto_title
+    controller = params[:controller]
+    resource = controller.singularize
+    case params[:action]
+    when "new"
+      action = "New"
+    when "edit"
+      action = "Editing"
+    when "show"
+      action = "Viewing"
+    when "index"
+      action = "Viewing"
+      resource = controller
+    else
+      action = "A better resume"
+      resource = ""
+    end
+    content_for :title, action + " " + resource
+  end
+
+  def page_title(text)
+    content_for :title, text.to_s
+  end
 end

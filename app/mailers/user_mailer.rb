59aacd0bfc43e0697a53546e9917d1c3f9a525e8
Kyle Jacobson <ksj@twelve8.net>
2013-07-01 21:59:24 -0700

    fix #88. from line of email should now say 'Re-CV.com'

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,17 @@
 class UserMailer < ActionMailer::Base
-  default :from => "mailer@re-cv.com"
+  require 'mail'
+  #default :from => "mailer@re-cv.com"
+
+  def from_address
+    address = Mail::Address.new "mailer@re-cv.com"
+    address.display_name = "Re-CV.com"
+    address.format
+  end
 
   def login_email(user, validation)
     @user = user
     @validation = validation
     return if @validation.user_id != @user.id
-    mail(:to => @user.email, :subject => "Your re-cv.com login link")
+    mail(:from => from_address, :to => @user.email, :subject => "Your re-cv.com login link")
   end
 end
